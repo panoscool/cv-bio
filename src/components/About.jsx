dcda821d5f4d708c74383417a7cd4131070ddf9f
Panos K <lkullolli@gmail.com>
2019-08-08 13:55:18 +0400

    about page wip

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Paper } from "@material-ui/core";
+import { Paper, Grid } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -10,6 +10,9 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(2),
     marginBottom: 16,
     color: theme.palette.text.secondary
+  },
+  namePadding: {
+    paddingTop: "2em"
   }
 }));
 
@@ -18,7 +21,27 @@ function About() {
 
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper}>about me</Paper>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <div style={{ textAlign: "center" }}>
+              <img
+                src="/assets/images/avatar.png"
+                alt="avatar"
+                className="avatar-img"
+              />
+            </div>
+            <h2 className={classes.namePadding}>Panos K.</h2>
+            <h4>Front End Developer</h4>
+            <hr className="about-line" />
+          </Grid>
+          <Grid item xs={8}>
+            <h2>Experience</h2>
+            <h2>Education</h2>
+            <h2>Skills</h2>
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 }
