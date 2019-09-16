c3824809f07beba2a04424941ce8cb9f63de2d1d
Panos K <lkullolli@gmail.com>
2019-09-16 16:01:14 +0400

    updated logo with setTimeout

--- src/components/Navigation/AppNavbar.jsx
+++ src/components/Navigation/AppNavbar.jsx
@@ -1,14 +1,6 @@
 import React, { Fragment, useState } from "react";
 import { Link, withRouter } from "react-router-dom";
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Hidden,
-  Drawer,
-  CssBaseline
-} from "@material-ui/core";
+import { AppBar, Toolbar, IconButton, Typography, Hidden, Drawer, CssBaseline } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Menu } from "@material-ui/icons";
 import LanguageSwitcher from "./LanguageSwitcher";
@@ -44,10 +36,17 @@ function AppNavbar({ location: { pathname } }) {
   const classes = useStyles();
 
   const [mobileOpen, setMobileOpen] = useState(false);
+  const [logoOpen, setLogoOpen] = useState(true)
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
+
+  const handleLogo = () => {
+    setLogoOpen(false)
+  }
+
+  setTimeout(() => handleLogo(), 3000)
 
   const styles = { textDecoration: "inherit", color: "inherit" };
 
@@ -57,21 +56,12 @@ function AppNavbar({ location: { pathname } }) {
 
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-          <IconButton
-            aria-label="open drawer"
-            onClick={handleDrawerToggle}
-            className={classes.navIconHide}
-          >
+          <IconButton aria-label="open drawer" onClick={handleDrawerToggle} className={classes.navIconHide}>
             <Menu />
           </IconButton>
           <Typography variant="h6" className={classes.grow} noWrap>
             <Link to="/" style={styles}>
-              <img
-                src="/assets/images/logo.png"
-                alt="PanosCool"
-                width="105px"
-                height="35px"
-              />
+              {logoOpen ? <img src="/assets/images/logo.png" alt="PanosCool" width="105px" height="35px" /> : <span>Panos K.</span>}
             </Link>
           </Typography>
           <ListMenu />
@@ -83,17 +73,9 @@ function AppNavbar({ location: { pathname } }) {
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
-          classes={{
-            paper: classes.drawerPaper
-          }}
-          ModalProps={{
-            keepMounted: true
-          }}
-        >
-          <DrawerMenu
-            pahname={pathname}
-            handleDrawerToggle={handleDrawerToggle}
-          />
+          classes={{ paper: classes.drawerPaper }}
+          ModalProps={{ keepMounted: true }} >
+          <DrawerMenu pahname={pathname} handleDrawerToggle={handleDrawerToggle} />
         </Drawer>
       </Hidden>
     </Fragment>
