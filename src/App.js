c56c625dffc31ca37b977c13cf11869cd51ca55c
Panos K <lkullolli@gmail.com>
2019-09-22 22:23:36 +0400

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, withRouter } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Layout from "./components/Layout/PageLayout";
 
@@ -21,4 +21,4 @@ function App() {
   );
 }
 
-export default withRouter(App);
+export default App;
